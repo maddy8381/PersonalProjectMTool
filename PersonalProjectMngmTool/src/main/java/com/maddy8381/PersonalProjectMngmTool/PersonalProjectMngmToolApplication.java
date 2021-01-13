934740e0209b77fe6c1732a0a59bbd1dcc460229
unknown <sgmadankar@gmail.com>
2021-01-14 00:45:23 +0530

    First Commit

---
+++ PersonalProjectMngmTool/src/main/java/com/maddy8381/PersonalProjectMngmTool/PersonalProjectMngmToolApplication.java
@@ -0,0 +1,13 @@
+package com.maddy8381.PersonalProjectMngmTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectMngmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectMngmToolApplication.class, args);
+	}
+
+}
