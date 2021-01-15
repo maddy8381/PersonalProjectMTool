3a25304c802e4aee598ef2d49c3e490ab47d9cfb
unknown <sgmadankar@gmail.com>
2021-01-15 21:13:40 +0530

    Added handling of Invalid Object error by using @Valid & @BindingResult

--- PersonalProjectMngmTool/src/main/java/com/maddy8381/PersonalProjectMngmTool/web/ProjectController.java
+++ PersonalProjectMngmTool/src/main/java/com/maddy8381/PersonalProjectMngmTool/web/ProjectController.java
@@ -7,10 +7,13 @@ import com.maddy8381.PersonalProjectMngmTool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,7 +25,13 @@ public class ProjectController {
 
     //ResponseEntity allows us to control resp statuses & JSON obj tht we r going to pass onto Client
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+        //@Valid for good error message if invalid data
+        //BindingResult is an Interface tht invokes validator on an object.
+
+        if(result.hasErrors()){
+            return new ResponseEntity<String>( "Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
