e1919864fe97acc62a9f4492cfb81d64d39dd1e7
unknown <sgmadankar@gmail.com>
2021-01-15 23:14:25 +0530

    If multiple UniqueId - throws error - to remove that error and show message properly, created CustomException and throwing that exception

--- PersonalProjectMngmTool/src/main/java/com/maddy8381/PersonalProjectMngmTool/services/ProjectService.java
+++ PersonalProjectMngmTool/src/main/java/com/maddy8381/PersonalProjectMngmTool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.maddy8381.PersonalProjectMngmTool.services;
 
 import com.maddy8381.PersonalProjectMngmTool.domain.Project;
+import com.maddy8381.PersonalProjectMngmTool.exceptions.ProjectIdException;
 import com.maddy8381.PersonalProjectMngmTool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,14 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            //Calling Custom Created Exception
+            throw new ProjectIdException("Project Id "+ project.getProjectIdentifier().toUpperCase() + " already exists");
+        }
 
-        //Logic
-        return projectRepository.save(project);
     }
 }
 
