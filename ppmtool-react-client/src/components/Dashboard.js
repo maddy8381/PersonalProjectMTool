189b0abde7280b7d43da4bae38db30b59090e11e
unknown <sgmadankar@gmail.com>
2021-01-16 15:14:18 +0530

    Project & Header Component Created .... Branch13

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
   render() {
     return (
-      <h1>Welcome to the Dashboard</h1>
+     <React.Fragment>
+        <h1>Welcome to the Dashboard</h1>
+        <ProjectItem />
+      </React.Fragment>
     );
   }
 }
