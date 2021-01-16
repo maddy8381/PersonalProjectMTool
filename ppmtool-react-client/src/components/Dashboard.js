bdca7861ae02067a6e248602135684e10e146b6e
unknown <sgmadankar@gmail.com>
2021-01-16 15:21:17 +0530

    Installed bootstrap by using npm & imported it into App.js

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -5,7 +5,7 @@ class Dashboard extends Component {
   render() {
     return (
      <React.Fragment>
-        <h1>Welcome to the Dashboard</h1>
+        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
         <ProjectItem />
       </React.Fragment>
     );
