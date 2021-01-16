d19703e4a1f4e61ca91ec98b051334fdc70af090
unknown <sgmadankar@gmail.com>
2021-01-16 17:04:47 +0530

    installed react-router-dom and added routing

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
-
+import {BrowserRouter as Router, Route} from "react-router-dom"; 
+import AddProject from './components/Project/AddProject';
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+          <Header />
+          <Route exact path="/dashboard"  component={Dashboard}/>
+          <Route exact path="/addProject"  component={AddProject}/>
+      </div>
+    </Router>
   );
 }
 
