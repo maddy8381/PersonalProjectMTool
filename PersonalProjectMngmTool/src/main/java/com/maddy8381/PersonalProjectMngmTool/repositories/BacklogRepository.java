8f1eb17d865730947108ba1983eca07c7df1df1b
unknown <sgmadankar@gmail.com>
2021-01-23 14:26:06 +0530

    Backlog class & ProjectTask class added in backend, Created Repo for them...branch31

---
+++ PersonalProjectMngmTool/src/main/java/com/maddy8381/PersonalProjectMngmTool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package com.maddy8381.PersonalProjectMngmTool.repositories;
+
+import com.maddy8381.PersonalProjectMngmTool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+}
