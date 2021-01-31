5aa96103360637bf7b85b82953d55be270883842
unknown <sgmadankar@gmail.com>
2021-01-31 20:20:56 +0530

    Handling UserName already found exception to show msg that username already exists...Branch62

--- PersonalProjectMngmTool/src/main/java/com/maddy8381/PersonalProjectMngmTool/services/UserService.java
+++ PersonalProjectMngmTool/src/main/java/com/maddy8381/PersonalProjectMngmTool/services/UserService.java
@@ -1,6 +1,7 @@
 package com.maddy8381.PersonalProjectMngmTool.services;
 
 import com.maddy8381.PersonalProjectMngmTool.domain.User;
+import com.maddy8381.PersonalProjectMngmTool.exceptions.UserNameAlreadyExistsException;
 import com.maddy8381.PersonalProjectMngmTool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,12 +17,18 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder; //for encoding password
 
     public User saveUser(User newUser){
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-        //Username must be unique
-        //Make sure that password & confirm password match
-        //We dont persist or show confirm password
-        return userRepository.save(newUser);
+        try{
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+            //Username must be unique
+            newUser.setUsername(newUser.getUsername()); //Incase username exists then throw UserNameAlreadyExistsException
+            //Make sure that password & confirm password match
+            //We dont persist or show confirm password
+            return userRepository.save(newUser);
+        }catch (Exception e){
+            throw new UserNameAlreadyExistsException("Username "+ newUser.getUsername() + " already exists");
+        }
     }
 
 }
