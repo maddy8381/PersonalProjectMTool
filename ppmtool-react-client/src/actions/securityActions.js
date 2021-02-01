ed5cd4d1036c1d8e6446d852a4467cf75cfe624b
unknown <sgmadankar@gmail.com>
2021-02-02 00:01:35 +0530

    SecurityActions and SecurityReducer added -- branch75

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async dispatch => {
 
@@ -10,6 +12,32 @@ export const createNewUser = (newUser, history) => async dispatch => {
             type: GET_ERRORS,
             payload: {}
         });
+    } catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        });
+    }
+};
+
+export const login = LoginRequest => async dispatch => {
+    try {
+        // post => Login Request
+        const res = await axios.post("/api/users/login", LoginRequest);
+        // Extract token from res.data
+        const { token } = res.data;
+        // Store the token in localstorage
+        localStorage.setItem("jwtToken", token);
+        // Set our token in header *** to pass as a authorization token for subsequent requests.
+        setJWTToken(token);
+        // decode the token on react
+        const decoded = jwt_decode(token);
+        // dispatch to our securityReducer
+        dispatch({
+            type: SET_CURRENT_USER,
+            payload: decoded
+        });
+
     } catch (err) {
         dispatch({
             type: GET_ERRORS,
